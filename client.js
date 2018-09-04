90cb4da007ad3d914e1cc7f1fb7832a3cea65bfd
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-04 13:24:29 -0500

    add generate button functionality

--- client.js
+++ client.js
@@ -1,8 +1,16 @@
 console.log('client.js');
 
+let buttonCounter = 0;
+
 $(document).ready(onReady);
 
 function onReady() {
   console.log('jQuery');
-  
+  $('#generateDiv').on('click', generateDiv);
+}
+
+function generateDiv() {
+  buttonCounter++;
+  let buttons = '<button class="swapButton">Swap</button><button class="deleteButton">Delete</button>';
+  $('body').append(`<div class="generatedDiv" id="${buttonCounter}"><p>${buttonCounter}</p>${buttons}</div>`);
 }
