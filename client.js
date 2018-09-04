eb47da66faf852873c923f716ef919291f0a47e7
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-04 13:40:58 -0500

    add swap and delete functionality

--- client.js
+++ client.js
@@ -7,10 +7,28 @@ $(document).ready(onReady);
 function onReady() {
   console.log('jQuery');
   $('#generateDiv').on('click', generateDiv);
+  $('#divOutput').on('click', '.swapButton', toggleBackground);
+  $('#divOutput').on('click', '.deleteButton', deleteDiv);
 }
 
+//increment buttonCounter, create new div with button counter as <p>, swap button, and delete button
 function generateDiv() {
   buttonCounter++;
-  let buttons = '<button class="swapButton">Swap</button><button class="deleteButton">Delete</button>';
-  $('body').append(`<div class="generatedDiv" id="${buttonCounter}"><p>${buttonCounter}</p>${buttons}</div>`);
+  let buttons = `
+    <button class="swapButton">Swap</button>
+    <button class="deleteButton">Delete</button>
+    `;
+  $('#divOutput').append(`<div class="generatedDiv"><p>${buttonCounter}</p>${buttons}</div>`);
+}
+
+function toggleBackground() {
+  //select div in which swap button is located, toggleClass '.yellowDiv'
+  let thisDiv = $(this).closest('div');
+  thisDiv.toggleClass('yellowDiv');
+}
+
+//remove div when delete button is clicked
+function deleteDiv() {
+  let thisDiv = $(this).closest('.generatedDiv');
+  thisDiv.remove();
 }
